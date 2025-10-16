eafca0ceb3fee9de0cfb16298d00e28b848ba4b9
TopalMahmutAli <mhmt_ali95@hotmail.com>
2025-10-16 09:52:55 +0200

    feat(frontend): update axios client + profile page + add profile.css

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://hackathon-livid-eight.vercel.app/api",
-  withCredentials: true,
+  //withCredentials: true,
 });
 
 export default API;
