e9656b52a24c48266f9f4d0c15ad03a000442c40
kao-outar <kaoutar.arare@gmail.com>
2025-10-17 12:20:17 +0200

    fix probleme backend!!)

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://projet-hackathon-z48d.vercel.app/api",
   //withCredentials: true,
 });
 
