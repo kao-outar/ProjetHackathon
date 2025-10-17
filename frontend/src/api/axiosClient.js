92bc2bc6581eb12b514da127f126d393c5d4aa4d
kao-outar <kaoutar.arare@gmail.com>
2025-10-17 11:08:24 +0200

    Fix: Configure Vercel rewrites to fix production CORS issue

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://hackathon-livid-eight.vercel.app/api",
+  baseURL: "/api",
   //withCredentials: true,
 });
 
